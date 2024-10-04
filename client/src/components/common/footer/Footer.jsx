8ca94d79a7c12c75bcc5fd1a6f9042e3712f1fd1
Chamara Sameera <sameerachamara10@gmail.com>
2024-10-04 14:10:02 +0530

    services page change

--- client/src/components/common/footer/Footer.jsx
+++ client/src/components/common/footer/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
         <div className='container'>
           <div className='box'>
             <div className='logo'>
-              <img src='../images/logo-light.png' alt='' />
+              <img src='../images/Zmedia Logo_07.png' alt='' />
               <h2>Do You Need Help With Anything?</h2>
               <p>Receive updates, hot deals, tutorials, discounts sent straignt in your inbox every month</p>
 
@@ -45,7 +45,7 @@ const Footer = () => {
         </div>
       </footer>
       <div className='legal'>
-        <span>© 2021 RentUP. Designd By GorkCoder.</span>
+        <span>© 2024 RentUP. Designd By Zmedia Digital.com.</span>
       </div>
     </>
   )
